f36b564dd3452093b56b6b4a925ed5654e44c03b
Rahul <rsinghbuddy@gmail.com>
2022-10-27 01:45:33 +0530

    added supabase api

---
+++ src/api/supabase/supabase.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { API_KEY } from '../../constants/apiConstants'
+
+const supabase = axios.create({
+    baseURL: 'https://dkgicggupnrxldwvkeft.supabase.co/',
+    headers: {
+        Authorization: `Bearer ${API_KEY}`
+    }
+})
+
+export default supabase
