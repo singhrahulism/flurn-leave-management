a0ae64064aa6125d282040545f71716d32800fd4
Rahul <rsinghbuddy@gmail.com>
2022-10-28 03:15:49 +0530

    added Floating Action Button

--- src/components/buttons/FloatingActionButton.js
+++ src/components/buttons/FloatingActionButton.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Octicons } from '@expo/vector-icons';
 
 const FloatingActionButton = () => {
-    return <View style={styles.container}>
-        <Text>BTN</Text>
-    </View>
+
+    const navigation = useNavigation()
+
+    const handlePress = () => {
+        navigation.navigate('CreateLeave')
+    }
+
+    return <TouchableOpacity style={styles.container} activeOpacity={0.75} onPress={handlePress} >
+        <Octicons name="plus" size={24} color="white" />
+    </TouchableOpacity>
 }
 
 const styles = StyleSheet.create({
     container: {
-    
+        position: 'absolute',
+        bottom: 15,
+        right: 15,
+        backgroundColor: '#20ad45',
+        height: 60,
+        width: 60,
+        borderRadius: 60/2,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
 
